4b6b461265e166ef677240d86574a9f618f83cb7
MartaNS09 <martacrystal9@gmail.com>
2025-10-03 16:43:48 +0300

    feat: добавить простую страницу карьеры

---
+++ src/pages/careers/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Careers() {
+  return (
+    <div style={{padding: '50px', textAlign: 'center'}}>
+      <h1>Карьера в NEROX</h1>
+      <p>Мы всегда рады талантливым специалистам!</p>
+      <p>Отправьте резюме на hr@nerox.ru</p>
+    </div>
+  );
+}
