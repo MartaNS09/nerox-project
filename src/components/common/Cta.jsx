87a604f7f5a9b43ea14f7ca012da395891a47a3b
MartaNS09 <martacrystal9@gmail.com>
2025-10-04 13:56:01 +0300

    Перевод футера на русский язык

--- src/components/common/Cta.jsx
+++ src/components/common/Cta.jsx
@@ -1,17 +1,26 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 const Cta = ({ padd }) => {
   return (
     <>
-      <div className={`tp-photograper-cta-2 pt-55 pb-55 ${padd ? padd : ''}`}>
+      <div className={`tp-photograper-cta-2 pt-55 pb-55 ${padd ? padd : ""}`}>
         <div className="container">
           <div className="row">
             <div className="col-xl-12">
               <div className="tp-photograper-cta-wrapper text-center">
-                <h3 className="tp-photograper-cta-title mb-20"><b>Can't find what you are looking for?</b></h3>
-                <p>We are always looking for people with unique skills. Send us your CV and we will contact you<br /> when we have a vacancy that meets your expectations.</p>
+                <h3 className="tp-photograper-cta-title mb-20">
+                  <b>Не нашли то, что искали?</b>
+                </h3>
+                <p>
+                  Мы всегда в поиске людей с уникальными навыками. Отправьте нам
+                  своё резюме, и мы свяжемся с вами,
+                  <br /> как только появится вакансия, соответствующая вашим
+                  ожиданиям.
+                </p>
                 <div className="tp-cta-btn mt-25">
-                  <Link href="/contact"><a className="tp-solid-btn-square">Contact us</a></Link>
+                  <Link href="/contact">
+                    <a className="tp-solid-btn-square">Связаться с нами</a>
+                  </Link>
                 </div>
               </div>
             </div>
--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,25 +1,36 @@
-import Link from 'next/link';
-import React from 'react';
-import { useFormik } from 'formik';
-import * as Yup from 'yup';
-import ErrorMsg from '../common/ErrorMsg';
-import schema from '../common/schema';
+import Link from "next/link";
+import React from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import ErrorMsg from "../common/ErrorMsg";
+import schema from "../common/schema";
 
 const Footer = () => {
   const handleOnSubmit = (values, { resetForm }) => {
-    alert(`${values.name + "\n" + values.email + "\n" + values.subject + "\n" + values.msg}`);
-    resetForm()
-  }
-  const { handleChange, handleSubmit, handleBlur, errors, values, touched } = useFormik({
-    initialValues: {
-      name: '',
-      email: '',
-      subject: '',
-      msg: ''
-    },
-    validationSchema: schema,
-    onSubmit: handleOnSubmit,
-  })
+    alert(
+      `${
+        values.name +
+        "\n" +
+        values.email +
+        "\n" +
+        values.subject +
+        "\n" +
+        values.msg
+      }`
+    );
+    resetForm();
+  };
+  const { handleChange, handleSubmit, handleBlur, errors, values, touched } =
+    useFormik({
+      initialValues: {
+        name: "",
+        email: "",
+        subject: "",
+        msg: "",
+      },
+      validationSchema: schema,
+      onSubmit: handleOnSubmit,
+    });
   return (
     <>
       <footer>
@@ -29,28 +40,43 @@ const Footer = () => {
               <div className="col-xl-6 col-lg-6 col-md-6">
                 <div className="footer__widget footer-col-1">
                   <div className="tp-section-title">
-                    <span className="tp-sub-title mb-15">#Contact Information</span>
-                    <h2 className="tp-title tp-title-df mb-20">Ask a question</h2>
+                    <span className="tp-sub-title mb-15">
+                      #Контактная информация
+                    </span>
+                    <h2 className="tp-title tp-title-df mb-20">
+                      Задайте вопрос
+                    </h2>
                   </div>
                   <div className="footer__lists mt-40">
-                    <div className="footer__list-item mb-40">
+                    {/* Адрес закомментирован */}
+                    {/* <div className="footer__list-item mb-40">
                       <div className="footer__list-icon">
                         <i className="fa-solid fa-location-dot"></i>
                       </div>
                       <div className="footer__list-text">
                         <p>
-                          <a href="https://goo.gl/maps/iAY7xEk5PGbqwBWf6" target="blank">
-                            Ta-134/A, Gulshan Badda Link Rd, Nya 10982 USA </a>
+                          <a
+                            href="https://goo.gl/maps/iAY7xEk5PGbqwBWf6"
+                            target="blank"
+                          >
+                            Ta-134/A, Gulshan Badda Link Rd, Nya 10982 USA{" "}
+                          </a>
                         </p>
                       </div>
-                    </div>
+                    </div> */}
                     <div className="footer__list-item mb-40">
                       <div className="footer__list-icon">
                         <i className="fas fa-envelope"></i>
                       </div>
                       <div className="footer__list-text">
-                        <p><a href="mailto:rasalinawillam@gmail.com">rasalinawillam@gmail.com</a></p>
-                        <p><a href="mailto:info@gmail.com">info@gmail.com</a></p>
+                        <p>
+                          <a href="mailto:rasalinawillam@gmail.com">
+                            rasalinawillam@gmail.com
+                          </a>
+                        </p>
+                        <p>
+                          <a href="mailto:info@gmail.com">info@gmail.com</a>
+                        </p>
                       </div>
                     </div>
                     <div className="footer__list-item mb-40">
@@ -58,8 +84,13 @@ const Footer = () => {
                         <i className="fa-solid fa-phone-flip"></i>
                       </div>
                       <div className="footer__list-text">
-                        <p><a href="tel:+08987878773345">+08 98787 8773 345</a></p>
-                        <p><a href="tel:+08987878773345">+08 98787 8773 345</a></p>
+                        {/* Ваш реальный номер телефона */}
+                        <p>
+                          <a href="tel:+375445777724">+375 (44) 577-77-24</a>
+                        </p>
+                        <p>
+                          <a href="tel:+375445777724">+375 (44) 577-77-24</a>
+                        </p>
                       </div>
                     </div>
                   </div>
@@ -68,24 +99,48 @@ const Footer = () => {
               <div className="col-xl-6 col-lg-6 col-md-6">
                 <div className="footer__widget footer-col-2">
                   <div className="tp-section-title">
-                    <span className="tp-sub-title mb-15">#Contact us</span>
-                    <h2 className="tp-title tp-title-df mb-20">Write to us</h2>
+                    <span className="tp-sub-title mb-15">
+                      #Свяжитесь с нами
+                    </span>
+                    <h2 className="tp-title tp-title-df mb-20">Напишите нам</h2>
                   </div>
                   <form id="contact-form" action="mail.php" method="POST">
                     <div className="contact-filed mb-20">
-                      <input id='name' value={values.name} onChange={handleChange} onBlur={handleBlur} type="text" placeholder="Name" />
+                      <input
+                        id="name"
+                        value={values.name}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        type="text"
+                        placeholder="Имя"
+                      />
                       {touched.name && <ErrorMsg error={errors.name} />}
                     </div>
                     <div className="contact-filed mb-30">
-                      <input id='email' value={values.email} onChange={handleChange} onBlur={handleBlur} type="email" placeholder="Email" />
+                      <input
+                        id="email"
+                        value={values.email}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        type="email"
+                        placeholder="Email"
+                      />
                       {touched.email && <ErrorMsg error={errors.email} />}
                     </div>
                     <div className="contact-filed mb-25">
-                      <textarea id='msg' value={values.msg} onChange={handleChange} onBlur={handleBlur} placeholder="Write a question"></textarea>
+                      <textarea
+                        id="msg"
+                        value={values.msg}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        placeholder="Напишите ваш вопрос"
+                      ></textarea>
                       {touched.msg && <ErrorMsg error={errors.msg} />}
                     </div>
                     <div className="form-submit">
-                      <button className="tp-grd-btn" type="submit">Send</button>
+                      <button className="tp-grd-btn" type="submit">
+                        Отправить
+                      </button>
                     </div>
                     <p className="ajax-response"></p>
                   </form>
@@ -98,12 +153,27 @@ const Footer = () => {
           <div className="container">
             <div className="row align-items-center">
               <div className="col-xl-6 col-lg-7">
-                <p>© 2022 Nerox - Web development studio. All rights reserved.</p>
+                {/* Изменено: "Студия веб-разработки" на более подходящие варианты */}
+                <p>
+                  © 2025 APSOD - Веб-разработка и digital-решения. Все права
+                  защищены.
+                </p>
+                {/* Добавлена информация об ИП */}
+                <div
+                  style={{
+                    fontSize: "14px",
+                    marginTop: "5px",
+                    opacity: "0.8",
+                    lineHeight: "1.4",
+                  }}
+                >
+                  <p>ИП Карелин Д.В., УНП 391853923</p>
+                </div>
               </div>
               <div className="col-xl-6 col-lg-5">
                 <div className="copyright-links">
                   <Link href="/privacy">
-                    <a>Privacy Policy</a>
+                    <a>Политика конфиденциальности</a>
                   </Link>
                 </div>
               </div>
